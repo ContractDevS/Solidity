a156b452cea7da319baa9c310f1a18ac07157daa
Sergey <57293178+OnCeDS@users.noreply.github.com>
2019-11-08 13:20:15 +0300

    Update LogicalSubmissionSystem.sol

--- LogicalSubmissionSystem.sol
+++ LogicalSubmissionSystem.sol
@@ -2,21 +2,22 @@ pragma solidity >= 0.4.0 < 0.6.0;
 
     contract mailService {
         
-        constructor() public{
+        constructor() public {
             roles[0] = "SysAdmin";   
             roles[1] = "Admin";   
             roles[2] = "Postman";
             roles[3] = "User"; 
-            _creatUser("SysAdmin", 1, 0, msg.sender, "" );
-            _creatUser("Admin", 1 , 1, address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C), "");
-            _creatUser("Postman", 2, 2, address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB), "141");
-            _creatUser("User", 3, 3, address(0x583031D1113aD414F02576BD6afaBfb302140225), "");
-            
+            _creatUser("Калтахин Василий Викторович", "г. Дмитров, ул. Инженерная, д.5", 0, msg.sender, "" );
+            _creatUser("Говтян Зухра Копрофиловна", "г. Дмитров, ул. Маркова, д.33" , 1, address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C), "");
+            _creatUser("Петров Иван Андреевич", "г. Москва, Зубовский бульвар, д.2", 2, address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB), "141");
+            _creatUser("Шариков Нектар Садамазович", "г. Дмитров, ул. Маркова, д.8", 3, address(0x583031D1113aD414F02576BD6afaBfb302140225), "");
         }
+        
         modifier checkRole(uint _roles) {
             require(_roles == 0 || _roles == 1, "Acces error");
             _;
         }
+        
         modifier chechActive(address userAddr) {
             for(uint i=0; i<users.length; i++) {
             if(userAddr == users[i].user) {
@@ -25,8 +26,8 @@ pragma solidity >= 0.4.0 < 0.6.0;
             }
             _;
         }
-        struct mailing 
-        {
+        
+        struct mailing {
             string trackNumber;
             address recipient;
             uint types;
@@ -38,48 +39,48 @@ pragma solidity >= 0.4.0 < 0.6.0;
             uint destinationAddress;
             uint departureAddress;
         }
-        struct remittance 
-        {
+        
+        struct remittance {
             address payable sender;
             address payable recipientMoney;
             uint sumMoney;
             uint timeOfLife;
             bool status;
         }
-        struct roleAttributes
-        {
+        
+        struct roleAttributes {
             string name;
-            uint homeAddress;
+            string homeAddress;
             uint role;
             address user;
             string postalIndex;
             bool active;
         }
+        
         roleAttributes[] users;
         remittance[] transfers;
-        
         
         mapping(uint=>string) roles;
         mapping(address=>uint) getRole;
         mapping(uint=>uint[]) getPath;
         
-        function _creatUser(string memory _name, uint _homeAddress, uint _role, address _user, string memory _postalIndex) private {
+        function _creatUser(string memory _name, string memory _homeAddress, uint _role, address _user, string memory _postalIndex) private {
             users.push(roleAttributes(_name, _homeAddress, _role, _user, _postalIndex, true));
             getRole[_user] = _role;
         }
-        function chengePrivInfo(string calldata _name, uint _homeAddress, uint _id) external {
+        
+        function chengePrivInfo(string calldata _name, string calldata _homeAddress, uint _id) external {
             require(users[_id].user == msg.sender, "Wrong user"); 
             users[_id].name = _name;
             users[_id].homeAddress = _homeAddress;
         }
         
-        function pay(address payable  _to, uint256 _eth) public payable
-        {
+        function pay(address payable  _to, uint256 _eth) public payable {
             require (msg.value >= _eth);
             _to.transfer(_eth);
         }
-        function createByAdmin(string memory _name, uint _homeAddress, uint _role, address _user, string memory _postalIndex) public checkRole(getRole[msg.sender]) chechActive(msg.sender)
-        {        
+        
+        function createByAdmin(string memory _name, string memory _homeAddress, uint _role, address _user, string memory _postalIndex) public checkRole(getRole[msg.sender]) chechActive(msg.sender) {        
             if(_role == 2) {
                 require(1 == getRole[msg.sender], "User is not Admin");
                 _creatUser(_name, _homeAddress, _role, _user, _postalIndex);
@@ -89,6 +90,7 @@ pragma solidity >= 0.4.0 < 0.6.0;
                 _creatUser(_name, _homeAddress, 1, _user, "" );
             }
         }
+        
         function deactivateUser(uint _id) external chechActive(msg.sender) {
              if(users[_id].role == 1) {
                 require(0 == getRole[msg.sender], "User is not SysAdmin");
@@ -99,18 +101,21 @@ pragma solidity >= 0.4.0 < 0.6.0;
                 users[_id].active = false;
              }
         }
-        function registration(string calldata _name, uint _homeAddress) external {
+        
+        function registration(string calldata _name, string calldata  _homeAddress) external {
             for(uint i=0; i<users.length; i++) {
                 require(msg.sender != users[i].user, "Account already created");
             }
             _creatUser(_name, _homeAddress, 3, msg.sender, "");
         }
+        
         function createTransfer(address payable _recipientMoney, uint _sumMoney, uint _timeOfLife) external payable {
             require (msg.value >= _sumMoney, "not enoght money");
             uint deadLine = now;
             deadLine += _timeOfLife * 1 days;
             transfers.push(remittance(msg.sender, _recipientMoney, _sumMoney * 1 ether, _sumMoney, true));
         }
+        
         function getTransfer(uint _id) external {
             if(transfers[_id].timeOfLife <= now && transfers[_id].status == true) {
                 transfers[_id].status = false;
@@ -121,19 +126,23 @@ pragma solidity >= 0.4.0 < 0.6.0;
             msg.sender.transfer(transfers[_id].sumMoney);
             transfers[_id].status = false;
         }
+        
         function cancelTransfer(uint _id) external {
             require(transfers[_id].status == true, "Transfer is not active");
             require(transfers[_id].recipientMoney == msg.sender || transfers[_id].sender == msg.sender, "You doesn't have permision");
             transfers[_id].sender.transfer(transfers[_id].sumMoney);
             transfers[_id].status = false;
         }
-        function viewUser(uint _id) view public returns(string memory, uint, uint, address, string memory, bool) {
+        
+        function viewUser(uint _id) view public returns(string memory, string memory, uint, address, string memory, bool) {
             roleAttributes memory a = users[_id];
             return(a.name, a.homeAddress, a.role, a.user, a.postalIndex, a.active);
         }
+        
         function viewMoney() public view checkRole(getRole[msg.sender]) returns(uint)  {
             return address(this).balance;   
         }
+        
         function viewTransfer(uint _id) public view returns(address, address payable, uint, uint, bool) {
             remittance memory b = transfers[_id];
             return(b.sender, b.recipientMoney, b.sumMoney, b.timeOfLife, b.status);
